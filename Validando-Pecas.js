3fa0e2bc83b2d15436cee5cda755548dae11e8bf
alex <alexdez1994@gmail.com>
2021-10-12 19:59:34 -0300

    Salvando atividade realizada

---
+++ Validando-Pecas.js
@@ -0,0 +1,9 @@
+// Atividade - validando numeros de pecas 
+
+let  numeroPecas = ["Disco de freio" ,"Amortecedor","Virabrequim","Vela","Bateria","Radiador","Correia","Carter","Ignição","Embreagem"];
+
+if (numeroPecas.length < 10) {
+    console.log ("cadastro feito")
+} else {
+    console.log ("cadastro não realizado")
+}
